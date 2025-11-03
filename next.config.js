9f34c808ade883e49c7eb3222c073342bb19bf76
bsuvonov <b.suvonov2022@gmail.com>
2025-11-03 13:41:10 +0800

    fix: thumbnail

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-const isProd = process.env.NODE_ENV === 'production'
-const repo = 'challenge'
 
 module.exports = withNextra({
   images: { unoptimized: true },
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
   trailingSlash: true,
 })
