83539ca012a2b606bf749db70efe3a10df2cb3e0
Bunyod Suvonov <109164894+bsuvonov@users.noreply.github.com>
2025-11-03 08:26:42 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-// next.config.js
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 const isProd = process.env.NODE_ENV === 'production'
 const repo = 'challenge'
 
 module.exports = withNextra({
-  output: 'export',
   images: { unoptimized: true },
   basePath: isProd ? `/${repo}` : '',
   assetPrefix: isProd ? `/${repo}/` : '',
